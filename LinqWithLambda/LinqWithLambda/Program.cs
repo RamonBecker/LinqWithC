9bb518f3075bafb7801a6fee5ba0514309a0f67e
RamonBecker <ramonbecker68@gmail.com>
2021-08-15 21:50:35 -0300

    métodos Take e TakeWhile

--- LinqWithLambda/LinqWithLambda/Program.cs
+++ LinqWithLambda/LinqWithLambda/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace LinqWithLambda {
     class Program {
         static void Main(string[] args) {
-            ITest test = new TestLast();
+            ITest test = new TestTake();
             test.Test();
 
             Console.ReadLine();
---
+++ LinqWithLambda/LinqWithLambda/Tests/TestTake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWithLambda.Tests {
+    public class TestTake : ITest {
+        public void Test() {
+            var customers = DataBase.DataBase.GetCustomers();
+
+            var firstTenCustomer = customers.Take(10);
+
+
+            foreach (var customer in firstTenCustomer) {
+                Console.WriteLine(customer.nome);
+            }
+            Console.WriteLine("\n");
+            //var takeWhileCustomers = customers.TakeWhile(customer => customer.idade != 40);
+            var takeWhileCustomers = customers.TakeWhile(customer => !(customer.idade == 40));
+
+            foreach (var customer in takeWhileCustomers) {
+                Console.WriteLine("Take while: " + customer.nome + " idade:" + customer.idade);
+            }
+        }
+    }
+}
